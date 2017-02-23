ae64cf951252e4629babdbf3de0a44dfa14cb5f5
김지성 <jskim6687@paran.com>
2017-02-23 23:00:50 +0900

    file upload system commit

--- Monkey/src/Monkey/Controllers/HomeController.cs
+++ Monkey/src/Monkey/Controllers/HomeController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.AspNetCore.Hosting.Server;
 
 namespace Monkey.Controllers
 {
     public class HomeController : Controller
     {
+        private IHostingEnvironment _environment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ICollection<IFormFile> files) //2017.02.23 jskim add file upload system
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                }
+            }
+            return View();
+        }
+
         /*
         public IActionResult About()
         {
