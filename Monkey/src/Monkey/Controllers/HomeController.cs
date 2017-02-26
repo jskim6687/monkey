952dfae1a19db0246b21116e46f7177439efa76b
김지성 <jskim6687@paran.com>
2017-02-26 21:47:29 +0900

    Model Binding

--- Monkey/src/Monkey/Controllers/HomeController.cs
+++ Monkey/src/Monkey/Controllers/HomeController.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.PlatformAbstractions;
-using Microsoft.AspNetCore.Hosting.Server;
+using Monkey.Controllers;
+using Monkey.Models;
 
 namespace Monkey.Controllers
 {
     public class HomeController : Controller
     {
+    
         private IHostingEnvironment _environment;
 
         public HomeController(IHostingEnvironment environment)
@@ -20,12 +18,65 @@ namespace Monkey.Controllers
             _environment = environment;
         }
 
-
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Index(ICollection<IFormFile> files)
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                        var fileName = file.FileName;
+                        if (fileName.EndsWith("o"))
+                        {
+                            new Ofiles
+                            {
+                                name = fileName,
+                                stationName = fileName.Substring(0,4),
+                                gpsday = int.Parse(fileName.Substring(4,3)),
+                                order = int.Parse(fileName.Substring(7,1)),
+                                year = int.Parse(fileName.Substring(9,2))
+                            };
+                        }
+                        else if (fileName.EndsWith("n"))
+                        {
+                            new Ofiles
+                            {
+                                name = fileName,
+                                stationName = fileName.Substring(0, 4),
+                                gpsday = int.Parse(fileName.Substring(4, 3)),
+                                order = int.Parse(fileName.Substring(7, 1)),
+                                year = int.Parse(fileName.Substring(9, 2))
+                            };
+                        }
+                        else
+                        {
+
+                        }
+//                        await file.CopyToAsync(fileStream);
+                    }
+                }
+            }
+            if (files.Count > 0)
+            {
+                return View();
+                //return Info();
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        /*
         [HttpPost]
         public async Task<IActionResult> Index(ICollection<IFormFile> files) //2017.02.23 jskim add file upload system
         {
@@ -40,8 +91,8 @@ namespace Monkey.Controllers
                     }
                 }
             }
-            return View();
-        }
+            return Orbit(files);
+        }*/
 
         /*
         public IActionResult About()
---
+++ Monkey/src/Monkey/Models/RinexFiles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Monkey.Models
+{
+    public class Nfiles
+    {
+        public int year { get; set; }
+        public string name { get; set; }
+        public string stationName { get; set; }
+        public int gpsday { get; set; }
+        public int order { get; set; }
+    }
+
+    public class Ofiles
+    {
+        public int year { get; set; }
+        public string name { get; set; }
+        public string stationName { get; set; }
+        public int gpsday { get; set; }
+        public int order { get; set; }
+    }
+}
